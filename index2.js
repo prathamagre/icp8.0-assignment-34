13fcae82bbb9c5c15791ea0f367a6081915fc792
prathamagre <prathamvaishu@gmail.com>
2024-07-10 02:44:49 +0530

    key loop program added

---
+++ index2.js
@@ -0,0 +1,14 @@
+// Program to say Hello to all elements from any array use key of loop
+
+const students = [
+    "Riya",
+    "Manoj",
+    "Vaishali",
+    "Rashi",
+    "Vani"
+]
+
+for (const num of students)
+{
+    console.log(`hello ${num}`)
+}
